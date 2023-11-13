963737a448e0b50ec99cdd2e0266ea6f64886c51
Erik Bogdan <erik.bogdan@gmail.com>
2023-11-13 22:14:37 +0100

    Added vue3, tailwind, and test page

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-2xl">
+        Initialized Page
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+s
